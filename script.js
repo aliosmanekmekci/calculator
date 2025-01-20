83225ff913059373348b2aa28eb2ec7008e7c483
Ali Ekmekci <aliosmanekmekci@gmail.com>
2025-01-20 09:36:17 +0300

    Create buttonClick function to handle Number & Symbols Create handleNumber & handleSymbols functions Change HTML structure

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
 const screen = document.querySelector(".screen");
-/*
-Orada almis sayiyi buttona klik fonsiyonu atamis
-isNaN ise handleSymbol fonsiyonu yazmis degilse farkli
-*/
 
 function buttonClick(value) {
   isNaN(parseInt(value)) ? handleSymbol(value) : handleNumber(value);
